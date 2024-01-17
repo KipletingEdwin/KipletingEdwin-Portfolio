b26d8e504231c5a1309cc88894867850a97d8919
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-01-17 08:36:27 +0300

    add porfolio header

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+        <a className={styles.title} href="">Portfolio</a>
+        <div className={styles.container}>
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
