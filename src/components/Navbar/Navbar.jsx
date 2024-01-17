688fa62f44c7efa98ed68b101ee48781e6f9b342
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-01-17 09:48:29 +0300

    add responsive design to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Navbar.module.css';
+import menuIcon from '../../assets/nav/menuIcon.png';
+import closeIcon from '../../assets/nav/closeIcon.png';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+
   return (
     <nav className={styles.navbar}>
-        <a className={styles.title} href="">Portfolio</a>
-        <div className={styles.container}>
+        <a className={styles.title} href="/">Portfolio</a>
+        <div className={styles.menu}>
+          <img className={styles.menuBtn} src={menuOpen ? closeIcon : menuIcon} alt='menuBar' onClick={() => setMenuOpen(!menuOpen)}/>
+          <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)} >
+            <li> <a href='#about'></a> About </li>
+            <li> <a href='#experience'></a> Experience </li>
+            <li> <a href='#projects'></a> Projects </li>
+            <li> <a href='#contact'></a> Contact </li>
+          </ul>
 
         </div>
     </nav>
