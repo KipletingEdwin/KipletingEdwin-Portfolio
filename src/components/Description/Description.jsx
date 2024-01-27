0198fc0a80fd01eb7c8eb130fcdd7fe3e948bc19
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-01-27 21:11:40 +0300

    style the image

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './Description.module.css';
-import profile from '../../assets/desc/edwin.png';
+import profile from '../../assets/desc/edwin1.png';
 
 function Description() {
   return (
     <div className={styles.container}>
         <div className={styles.myDescription}> 
-        <h3> Hello, I'm Edwin</h3>
-        <p> I'm a full stack developer majoring in JavaScript,React,Ruby on Rails and SQL</p>
+        <h1> Hello, I'm Edwin</h1>
+        <p> I'm a full stack developer majoring in JavaScript,ReactJS,Ruby,Ruby on Rails and SQL</p>
         </div>
         <div className={styles.myImage}>
             <img src={profile} alt='myImage'/>
