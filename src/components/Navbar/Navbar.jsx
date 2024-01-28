043260434dd67775ed8c9bfe3616a70f0d2a59ec
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-01-28 09:54:58 +0300

    make my navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
 
   return (
     <nav className={styles.navbar}>
-        <a className={styles.title} href="/"> My Portfolio</a>
+        <a className={styles.title} href="/">Portfolio.</a>
         <div className={styles.menu}>
           <img className={styles.menuBtn} src={menuOpen ? closeIcon : menuIcon} alt='menuBar' onClick={() => setMenuOpen(!menuOpen)}/>
           <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)} >
