631252b381a0f21deb3b7649fc9cb618725f0747
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-01-28 12:33:23 +0300

    style the role section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,13 +6,13 @@ import frontend from "../../assets/about/frontend.png";
 
 function About() {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} id="about">
       <h2> About </h2>
       <div className={styles.aboutMe}>
-        <ul>
-          <li>
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
             <img src={frontend} alt="frontend" />
-            <div className={styles.content}>
+            <div className={styles.aboutItemText}>
               <h3> FrontEnd Developer</h3>
               <p>
                 I am a frontend developer majoring in JavaScript and React
@@ -20,9 +20,9 @@ function About() {
             </div>
           </li>
 
-          <li>
+          <li className={styles.aboutItem}>
             <img src={server} alt="backend" />
-            <div className={styles.content}>
+            <div className={styles.aboutItemText}>
               <h3> BackEnd Developer</h3>
               <p>
                 I am a backend developer majoring in Ruby and Ruby on Rails
