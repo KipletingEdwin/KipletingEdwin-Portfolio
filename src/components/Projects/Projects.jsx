58553fd2054026827af6b50c2e2e9cca0aa62bd4
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-01-29 19:56:07 +0300

    style my project card

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,29 +1,25 @@
-import styles from './Projects.module.css';
-import projects from '../../data/projects.json'
+import styles from "./Projects.module.css";
+import projects from "../../data/projects.json";
 
-import React from 'react'
+import React from "react";
 
 function Projects() {
   return (
     <section className={styles.container} id="projects">
-        <h2> Projects</h2>
-        <div className={styles.projects}>
-            {
-                projects.map((project, id) => {
-                    return <div key={id}>
-                        <img src={project.imageSrc} alt={`Image of ${project.title}`}/>
-                        <h3>{project.title}</h3>
-                        <p>{project.description}</p>
-                    </div>
-
-                })
-
-            }
-
-        </div>
-
+      <h2> Projects</h2>
+      <div className={styles.projects}>
+        {projects.map((project, id) => {
+          return (
+            <div key={id} className={styles.project}>
+              <img src={project.imageSrc} alt={`Image of ${project.title}`} />
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+            </div>
+          );
+        })}
+      </div>
     </section>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
