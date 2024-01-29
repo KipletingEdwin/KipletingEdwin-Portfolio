ccfc693c8b73ea9500abe56d801ea122c21927f7
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-01-29 19:22:23 +0300

    make my page scrollable

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,11 +1,26 @@
 import styles from './Projects.module.css';
+import projects from '../../data/projects.json'
 
 import React from 'react'
 
 function Projects() {
   return (
     <section className={styles.container} id="projects">
-        <h1> Projects</h1>
+        <h2> Projects</h2>
+        <div className={styles.projects}>
+            {
+                projects.map((project, id) => {
+                    return <div key={id}>
+                        <img src={project.imageSrc} alt={`Image of ${project.title}`}/>
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                    </div>
+
+                })
+
+            }
+
+        </div>
 
     </section>
   )
