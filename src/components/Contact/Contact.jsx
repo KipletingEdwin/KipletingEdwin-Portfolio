4236c1494c5d1d008a079a1eb6808414d707fd5f
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-01-30 11:35:34 +0300

    intergrate the email.js library

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,43 +1,68 @@
-import styles from './Contact.module.css';
+import styles from "./Contact.module.css";
 
-import React from 'react'
+import React from "react";
 
 function Contact() {
+
+    function sendEmail(e){
+        e.preventDefault();
+        let formData = {
+            name : e.target.name.value,
+            email : e.target.email.value,
+            subject : e.target.subject.value,
+            message : e.target.message.value,
+        };
+
+        emailjs.send('service_cxbqpxe', 'template_mbjhj3t', formData)
+        .then((response) => {
+            alert('Email sent successfully:', response);
+        })
+        .catch((error) => {
+            alert('Error sending email:', error);
+        });
+    }
+
+
   return (
     <div className={styles.container}>
-        <h3>Contact</h3>
-        <p> Feel free to reach out to me! </p>
-        <div className={styles.contactForm}>
-            <form className={styles.contactFormInputs}>
-                <ul>
-                    <li>
-                        <input type='text' name='name' placeholder='Name' required/>
-                    </li>
+      <h3>Contact</h3>
+      <p> Feel free to reach out to me! </p>
+      <div className={styles.contactForm}>
+        <form className={styles.contactFormInputs} onSubmit={sendEmail}>
+          <ul>
+            <li>
+              <input type="text" name="name" placeholder="Name" required />
+            </li>
 
-                    <li>
-                        <input type='email' name='email' placeholder='Email' required/>
-                    </li>
+            <li>
+              <input type="email" name="email" placeholder="Email" required />
+            </li>
 
-                    <li>
-                        <input type='text' name='subject' placeholder='Subject' required/>
-                    </li>
+            <li>
+              <input
+                type="text"
+                name="subject"
+                placeholder="Subject"
+                required
+              />
+            </li>
 
-                    <li>
-                         <textarea placeholder='Message' name='message' required></textarea>
-                    </li>
+            <li>
+              <textarea
+                placeholder="Message"
+                name="message"
+                required
+              ></textarea>
+            </li>
 
-                    <li>
-                        <input type='submit' className={styles.flatButton} value="SEND"/>
-                    </li>
-
-                   
-
-                </ul>
-            </form>
-
-        </div>
+            <li>
+              <input type="submit" className={styles.flatButton} value="SEND" />
+            </li>
+          </ul>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
