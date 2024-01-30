ed17a5d4fd1a258900cd693947568dc0a781eacc
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-01-30 16:43:55 +0300

    add media queries to project section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,39 +1,45 @@
 import styles from "./Contact.module.css";
 import React from "react";
+import contactImage from "../../assets/contact/contact.png";
 
 function Contact() {
+  function sendEmail(e) {
+    e.preventDefault();
 
-    function sendEmail(e){
-        e.preventDefault();
-
-        let formData = {
-            name : e.target.name.value,
-            email : e.target.email.value,
-            subject : e.target.subject.value,
-            message : e.target.message.value,
-        };
-        if (!formData.name || !formData.email || !formData.subject || !formData.message){
-            alert("Please fill out all fields");
-            return;
-        }
-
-        
-
-        emailjs.send('service_cxbqpxe', 'template_mbjhj3t', formData)
-        .then((response) => {
-            alert('Email sent successfully:', response);
-        })
-        .catch((error) => {
-            alert('Error sending email:', error);
-        });
+    let formData = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      subject: e.target.subject.value,
+      message: e.target.message.value,
+    };
+    if (
+      !formData.name ||
+      !formData.email ||
+      !formData.subject ||
+      !formData.message
+    ) {
+      alert("Please fill out all fields");
+      return;
     }
 
+    emailjs
+      .send("service_cxbqpxe", "template_mbjhj3t", formData)
+      .then((response) => {
+        alert("Email sent successfully:", response);
+      })
+      .catch((error) => {
+        alert("Error sending email:", error);
+      });
+  }
 
   return (
     <div className={styles.container}>
-      <h3>Contact</h3>
-      <p> Feel free to reach out to me! </p>
+      <div className={styles.containerImage}>
+        <h3>Contact</h3>
+        <img src={contactImage} alt="contact" />
+      </div>
       <div className={styles.contactForm}>
+        <p> Feel free to reach out to me! </p>
         <form className={styles.contactFormInputs} onSubmit={sendEmail}>
           <ul>
             <li>
