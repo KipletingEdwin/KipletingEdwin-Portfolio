0f28aeb2b77e05381920afdd77294c260d99b4f2
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-27 15:15:05 +0300

    make my name rotate every half a second

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Description.module.css";
 import profile from "../../assets/desc/edwin1.png";
 
 function Description() {
+
+  const [rotate, setRotate] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const sentance = "Hello, I'm Edwin";
+
+  useEffect(()=> {
+    const interval = setInterval(() => {
+      if (currentIndex < sentance.length){
+        setRotate((prevRotate) => prevRotate + sentance[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+
+      } else {
+        setCurrentIndex(0);
+        setRotate("");
+      }
+      
+    }, 500);
+    return () =>clearInterval(interval);
+  }, [currentIndex, sentance.length]);
+
+
+
+
   return (
     <div className={styles.container}>
       <div className={styles.myDescription}>
-        <h1> Hello, I'm Edwin</h1>
+        <h1> {rotate}</h1>
         <p>
           I'm a full stack developer majoring in JavaScript,ReactJS,Ruby,Ruby on
           Rails and SQL
